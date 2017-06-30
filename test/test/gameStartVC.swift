27ef011a47893f57e24208d40a87b7adde3ef604
HYT2016 <ios135@Mac-09.local>
2017-06-30 16:16:31 +0800

    ADD

--- test/test/gameStartVC.swift
+++ test/test/gameStartVC.swift
@@ -7,15 +7,42 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class gameStartVC: UIViewController {
-
-    var myTableView: UITableView!
+    
+    
+    @IBOutlet weak var TesttextView: UITextView!
+    
     
 
+    var version:[String] = ["口病","AA"]
     
     
-        
+    func loadJsonToArys(){
+        //read file
+        let filePath=Bundle.main.path(forResource: "data", ofType:
+            "json")
+        let data1:Data
+        let json_parsed:JSON
+        do{
+            
+            
+            try data1 = Data(contentsOf: URL(fileURLWithPath:
+                filePath!, isDirectory: false))
+            json_parsed=JSON(data: data1)
+            print(json_parsed["version"])
+            let aa =
+                json_parsed["version"].arrayValue.map{$0.stringValue}
+            print(aa)
+            self.version=aa
+        }catch{
+            print(error.localizedDescription)
+        }
+    }
+
+    
+    
     
     
     
@@ -30,7 +57,48 @@ class gameStartVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
+        TesttextView.text!=String(describing: version)
+        
+        TesttextView.backgroundColor = UIColor(red: 90/255, green: 30/255, blue: 30/255, alpha: 1)
+        TesttextView.font = UIFont.systemFont(ofSize: 20, weight: 20)
+        TesttextView.textColor = UIColor.white
+        TesttextView.font = UIFont.boldSystemFont(ofSize: 20)
+        TesttextView.font = UIFont(name: "Verdana", size: 17)
+//        Make web links clickable
+        TesttextView.isSelectable = true
+        TesttextView.isEditable = true
+        TesttextView.dataDetectorTypes = UIDataDetectorTypes.link
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,14 +107,4 @@ class gameStartVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
-
-}
