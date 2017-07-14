db43713dcc65b0674c7dd66252a28ade70e29398
HYT2016 <ios135@Mac-09.local>
2017-07-14 17:40:12 +0800

    add

--- test/test/inVC.swift
+++ test/test/inVC.swift
@@ -10,18 +10,6 @@ import UIKit
 
 class inVC: UIViewController {
 
-//    var gradientLayer: CAGradientLayer!
-    
-    
-//    func createGradientLayer() {
-//        gradientLayer = CAGradientLayer()
-//        
-//        gradientLayer.frame = self.view.bounds
-//        
-//        gradientLayer.colors = [UIColor.red.cgColor, UIColor.yellow.cgColor]
-//        
-//        self.view.layer.addSublayer(gradientLayer)
-//    }
 
     
     @IBAction func loginBtn(_ sender: UIButton) {
@@ -37,39 +25,18 @@ class inVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        createGradientLayer()
         
-        //        兩個漸層顏色
-        let color1 = UIColor(red: 115, green: 165, blue: 239, alpha: 1)
-        let color2 = UIColor(red: 67, green: 141, blue: 252, alpha: 1)
-        //      漸層的CAlayer
-        let gradient = CAGradientLayer()
-        gradient.frame = self.view.frame
-        gradient.colors = [color1.cgColor,color2.cgColor]
-        //      把漸層layer加入畫面中
-        self.view.layer.insertSublayer(gradient, at: 0)
-//        self.view.backgroundColor = UIColor.green
-
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
     
     
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
